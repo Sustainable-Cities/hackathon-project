ca77245d81095d049933120bda4d26a725eed4bb
David Martel <daveymartel@gmail.com>
2021-02-20 10:01:15 -0500

    user controller

---
+++ controllers/UserContr.js
@@ -0,0 +1,52 @@
+const CreateUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const password_digest = await hashPassword(password);
+    const user = await User.create({
+      email: email,
+      password_digest: password_digest,
+    });
+    res.send(user);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const LoginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { email: req.body.email },
+      raw: true,
+    });
+    if (
+      user &&
+      (await passwordValid(req.body.password, user.password_digest))
+    ) {
+      let payload = {
+        id: user.id,
+      };
+      let token = createToken(payload);
+      return res.send({ user, token });
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const RefreshSession = async (req, res) => {
+  try {
+    const { token } = res.locals;
+    const user = await User.findByPk(token.id, {
+      attributes: ["id"],
+    });
+    res.send({ user, status: "OK" });
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  CreateUser,
+  LoginUser,
+  RefreshSession,
+};
