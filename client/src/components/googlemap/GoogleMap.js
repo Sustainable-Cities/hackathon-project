36d03f87f990b78796771e7b46688025b64ba2ea
Evyn Newton <newton.evyn@gmail.com>
2021-02-20 13:43:35 -0500

    map rendering on homepage

---
+++ client/src/components/googlemap/GoogleMap.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
+
+export function GoogleMap(props) {
+  const [markers, setMarkers] = useState([]);
+  const [activeMarker, setActiveMarker] = useState({});
+  const [selectedPlace, setSelectedPlace] = useState({});
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
+  const { google } = props;
+
+  const onMarkerClick = (props, marker, e) => {
+    // console.log(props);
+    setSelectedPlace(props);
+    setActiveMarker(marker);
+    setShowingInfoWindow(true);
+  };
+
+  const onClose = () => {
+    if (showingInfoWindow) {
+      setShowingInfoWindow(false);
+      setActiveMarker(null);
+    }
+  };
+
+  return (
+    <Map
+      google={google}
+      zoom={14}
+      style={mapStyles}
+      initialCenter={{
+        lat: 42.3601,
+        lng: -71.0589,
+      }}
+    >
+      {markers.map((marker, index) => (
+        <Marker
+          position={{ lat: marker.lat, lng: marker.lng }}
+          name={marker.address}
+          onClick={onMarkerClick}
+          key={index}
+        />
+      ))}
+      <InfoWindow
+        marker={activeMarker}
+        visible={showingInfoWindow}
+        onClose={onClose}
+      >
+        <div>
+          <h4>{selectedPlace.name}</h4>
+        </div>
+      </InfoWindow>
+    </Map>
+  );
+}
+
+export default GoogleApiWrapper({
+  apiKey: `${process.env.REACT_APP_GOOGLEMAPS_KEY}`,
+  // markers: markers,
+})(GoogleMap);
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+import GoogleApiWrapper from "../googlemap/GoogleMap";
 
 const Home = () => {
-	return <div>Home</div>;
+  return (
+    <div>
+      <div>Home</div>
+      <GoogleApiWrapper />
+    </div>
+  );
 };
 
 export default Home;
