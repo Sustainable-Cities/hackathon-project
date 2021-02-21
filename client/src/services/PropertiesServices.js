d0d401f03639af4a15b72a221c16df2dd89503e1
David Martel <daveymartel@gmail.com>
2021-02-21 11:06:21 -0500

    property services

--- client/src/services/PropertiesServices.js
+++ client/src/services/PropertiesServices.js
@@ -1,1 +1,10 @@
 import ApiClient from "./ApiClient";
+
+export const __GetProperties = async () => {
+  try {
+    const res = await ApiClient.get("/");
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
