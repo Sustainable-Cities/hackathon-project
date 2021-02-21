88964c838b19d65466260a11b86fda21000f6b34
David Martel <daveymartel@gmail.com>
2021-02-21 10:53:54 -0500

    favs controller

--- controllers/FavContr.js
+++ controllers/FavContr.js
@@ -1,4 +1,4 @@
-const { Favorites, User } = require("../models");
+const { Favorites } = require("../models");
 
 const CreateFav = async (req, res) => {
   try {
@@ -12,6 +12,28 @@ const CreateFav = async (req, res) => {
   }
 };
 
+const DeleteFav = async (req, res) => {
+  try {
+    await Favorites.destroy({ where: { id: parseInt(req.params.fav_id) } });
+    res.send({ msg: "fav was deleted" });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const GetFavs = async (req, res) => {
+  try {
+    const favs = await Favorites.findAll({
+      where: { user_id: parseInt(req.params.user_id) },
+    });
+    res.send(favs);
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
   CreateFav,
+  DeleteFav,
+  GetFavs,
 };
