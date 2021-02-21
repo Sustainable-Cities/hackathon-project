ff80a42923c9bd84b7d73091abd75b6e3035549e
Evyn Newton <newton.evyn@gmail.com>
2021-02-21 16:26:17 -0500

    added typography in signup page

--- client/src/screens/signup/SignUp.jsx
+++ client/src/screens/signup/SignUp.jsx
@@ -1,48 +1,49 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import logo from '../../assets/LogoHolder.png'
-import loginImg from '../../assets/login-image.jpg'
+import logo from "../../assets/LogoHolder.png";
+import loginImg from "../../assets/login-image.jpg";
 import { __CreateUser } from "../../services/UserServices";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   signUp: {
     marginTop: "8vh",
-    display: 'grid',
-    gridTemplateColumns: '1fr 1fr',
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
   },
   color: {
-    backgroundColor: '#E5E5E5',
-    height: '100vh'
+    backgroundColor: "#E5E5E5",
+    height: "100vh",
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    marginTop: '10%'
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: "10%",
   },
   button: {
-    backgroundColor: '#0039A9',
-    color: 'white',
-    margin: '1em 2em',
-    borderRadius: '1em',
-    width: '30%',
-    height: '2.5em'
+    backgroundColor: "#0039A9",
+    color: "white",
+    margin: "1em 2em",
+    borderRadius: "1em",
+    width: "30%",
+    height: "2.5em",
   },
   text: {
-    height: '2.5em',
-    width: '30%',
-    margin: '.5em',
-    border: '1px solid transparent',
-    borderRadius: '1em',
-    backgroundColor: 'white',
-    boxShadow: 'inset 0 1px 2px rgba(0,0,0,.39), 0 -1px 1px #FFF, 0 1px 0 #FFF',
+    height: "2.5em",
+    width: "30%",
+    margin: ".5em",
+    border: "1px solid transparent",
+    borderRadius: "1em",
+    backgroundColor: "white",
+    boxShadow: "inset 0 1px 2px rgba(0,0,0,.39), 0 -1px 1px #FFF, 0 1px 0 #FFF",
   },
   title: {
-    fontWeight: '200',
-    fontSize: '2em',
-    marginBottom: '1em'
-  }
+    fontWeight: "200",
+    fontSize: "2em",
+    marginBottom: "1em",
+  },
 }));
 
 const SignUp = () => {
@@ -59,7 +60,7 @@ const SignUp = () => {
     event.preventDefault();
     if (formState.password === formState.re_password) {
       await __CreateUser(formState);
-        history.push("/home");
+      history.push("/home");
     }
     setFormState(initialState);
   };
@@ -69,51 +70,58 @@ const SignUp = () => {
   return (
     <div className={classes.signUp}>
       <div className={classes.color}>
-
-      <form className={classes.form} onSubmit={handleSubmit}>
-      <img src={logo} alt='logo' style={{width: '118px', height: '116px', marginBottom: '1em'}}/>
-      <div className={classes.title}>Sign Up!</div>
-        <input
-          id="username"
-          onChange={handleChange}
-          value={formState.username}
-          placeholder="Username"
-          className={classes.text}
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <img
+            src={logo}
+            alt="logo"
+            style={{ width: "118px", height: "116px", marginBottom: "1em" }}
           />
-        <br />
-        <input
-          id="email"
-          onChange={handleChange}
-          value={formState.email}
-          placeholder="Email"
-          type='email'
-          className={classes.text}
-          />
-        <br />
-        <input
-          id="password"
-          onChange={handleChange}
-          value={formState.password}
-          placeholder="Password"
-          type='password'
-          className={classes.text}
-          />
-        <br />
-        <input
-          id="re_password"
-          onChange={handleChange}
-          value={formState.re_password}
-          placeholder="Confirm Password"
-          type='password'
-          className={classes.text}
+          <Typography className={classes.title}>Sign Up!</Typography>
+          <input
+            id="username"
+            onChange={handleChange}
+            value={formState.username}
+            placeholder="Username"
+            className={classes.text}
           />
           <br />
-        <button type="submit" className={classes.button}>
-          Sign Up
-        </button>
-      </form>
-          </div>
-          <img src={loginImg} alt='login image' style={{width: '50vw', height:'100%'}} />
+          <input
+            id="email"
+            onChange={handleChange}
+            value={formState.email}
+            placeholder="Email"
+            type="email"
+            className={classes.text}
+          />
+          <br />
+          <input
+            id="password"
+            onChange={handleChange}
+            value={formState.password}
+            placeholder="Password"
+            type="password"
+            className={classes.text}
+          />
+          <br />
+          <input
+            id="re_password"
+            onChange={handleChange}
+            value={formState.re_password}
+            placeholder="Confirm Password"
+            type="password"
+            className={classes.text}
+          />
+          <br />
+          <button type="submit" className={classes.button}>
+            Sign Up
+          </button>
+        </form>
+      </div>
+      <img
+        src={loginImg}
+        alt="login image"
+        style={{ width: "50vw", height: "100%" }}
+      />
     </div>
   );
 };
