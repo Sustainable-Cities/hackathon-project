6af7d0a3c4f134c48d9db16969eb1c26fccd8624
David Martel <daveymartel@gmail.com>
2021-02-21 11:09:50 -0500

    user services

--- client/src/services/PropertiesServices.js
+++ client/src/services/PropertiesServices.js
@@ -2,7 +2,7 @@ import ApiClient from "./ApiClient";
 
 export const __GetProperties = async () => {
   try {
-    const res = await ApiClient.get("/");
+    const res = await ApiClient.get("/properties/");
     return res.data;
   } catch (error) {
     throw error;
--- client/src/services/UserServices.js
+++ client/src/services/UserServices.js
@@ -1,1 +1,28 @@
 import ApiClient from "./ApiClient";
+
+export const __CreateUser = async (form) => {
+  try {
+    const res = await ApiClient.post("/user/register", form);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __LoginUser = async (form) => {
+  try {
+    const res = await ApiClient.post("/user/login", form);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __CheckSession = async () => {
+  try {
+    const res = await ApiClient.get("/users/refresh/session");
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
