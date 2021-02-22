a5b2bd0fe5be8c622a79af17d6da1c380c17e396
David Martel <daveymartel@gmail.com>
2021-02-22 10:02:02 -0500

    favs api call

--- client/src/screens/favorites/Favorites.jsx
+++ client/src/screens/favorites/Favorites.jsx
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { __GetFavs } from "../../services/FavServices";
 
-export default function Favorites() {
+export default function Favorites({ loggedIn }) {
+  const [favs, setFavs] = useState([]);
+
+  useEffect(() => {
+    GetFavs();
+  }, []);
+
   // API CALL TO GET FAVS USING USER ID
+  const GetFavs = async () => {
+    const data = __GetFavs(loggedIn.id);
+    setFavs(data);
+  };
+
   return <div>Favorites Screen</div>;
 }
