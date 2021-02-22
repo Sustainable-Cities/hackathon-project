dcced92c6b7d22367894a004b3687f89dddd749b
Evyn Newton <newton.evyn@gmail.com>
2021-02-22 12:51:02 -0500

    details page

--- client/src/components/moredetails/MoreDetails.jsx
+++ client/src/components/moredetails/MoreDetails.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
-import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import StarIcon from "@material-ui/icons/Star";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  root: { width: "100vw", height: "100vh", background: "#EBECF0" },
+  paper: {
+    width: "80vw",
+    height: "85vh",
+    margin: "8vh auto 8vh auto",
+  },
+}));
 
 export default function MoreDetails(props) {
   const classes = useStyles();
   const { properties, addFav } = props;
   const params = useParams();
+  const property = properties.filter((item) => item.id === params.id);
 
   return (
-    <div>
-      <Paper>MORE DEETS</Paper>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Typography></Typography>MORE DEETS
+      </Paper>
     </div>
   );
 }
