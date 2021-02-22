0058104cf31bca566cf5b5e67faf824a60a11b2b
David Martel <daveymartel@gmail.com>
2021-02-22 13:57:19 -0500

    seed

--- seeders/20210220183406-data.js
+++ seeders/20210220183406-data.js
@@ -14,7 +14,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     let arr = [];
     let val = Object.values(json);
-    for (let i = 1500; i < 1540; i++) {
+    for (let i = 1540; i < 1580; i++) {
       let obj = JSON.parse(val[i]);
       const latLong = await getLatLong(obj.address);
       arr.push({
